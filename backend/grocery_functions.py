6c14498b3c58173e41f72549e6c8fa6b25d6c39d
Michelle Lee <michelles.lee3153@gmail.com>
2025-11-05 17:06:53 -0800

    starting to set up firebase

--- backend/grocery_functions.py
+++ backend/grocery_functions.py
@@ -1,7 +1,13 @@
+import firebase_admin
+from firebase_admin import firestore
+
+app = firebase_admin.initialize_app()
+db = firestore.client()
+
+
 
 class Food():
     pass
-
 
 def get_food():
     pass
@@ -18,3 +24,4 @@ def add_interval():
 
 if __name__ == "__main__":
     pass
+
